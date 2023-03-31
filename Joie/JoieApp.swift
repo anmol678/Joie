1ee9440d9d50663db3c6eb14c4676f04d5a26c10
Anmol Jain <anmolj678@gmail.com>
2023-03-31 01:28:30 -0700

    Initial Commit

---
+++ Joie/JoieApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoieApp.swift
+//  Joie
+//
+//  Created by Anmol Jain on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct JoieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
